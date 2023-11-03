bb4c5860b5e3596138db00a85fcbdc264d337443
Josh Pollock <jopo@mit.edu>
2023-11-03 18:50:00 -0400

    initial thoughts

--- src/withBluefish.tsx
+++ src/withBluefish.tsx
@@ -3,11 +3,13 @@ import {
   Component,
   JSX,
   createContext,
+  createMemo,
   createUniqueId,
   mergeProps,
   useContext,
 } from "solid-js";
 import { Id } from "./scenegraph";
+import { ParentScopeIdContext, ScopeContext } from "./createName";
 
 export type WithBluefishProps<T = object> = T & {
   name: Id;
@@ -21,17 +23,33 @@ export function withBluefish<ComponentProps>(
   WrappedComponent: Component<WithBluefishProps<ComponentProps>>
 ) {
   return function (props: Omit<ComponentProps, "name"> & { name?: Id }) {
+    // scenegraph id
     const contextId = useContext(IdContext);
     const genId = createUniqueId();
     const id = () => props.name ?? contextId() ?? genId;
 
+    // component scope id
+    const [scope, setScope] = useContext(ScopeContext);
+    const scopeId = createMemo(() => {
+      if (props.name !== undefined) {
+        return props.name;
+      }
+
+      // TODO: check if already defined?
+
+      setScope(genId, {});
+      return genId;
+    });
+
     return (
-      <IdContext.Provider value={id}>
-        <WrappedComponent
-          {...(props as WithBluefishProps<ComponentProps>)}
-          name={id()}
-        />
-      </IdContext.Provider>
+      <ParentScopeIdContext.Provider value={scopeId}>
+        <IdContext.Provider value={id}>
+          <WrappedComponent
+            {...(props as WithBluefishProps<ComponentProps>)}
+            name={id()}
+          />
+        </IdContext.Provider>
+      </ParentScopeIdContext.Provider>
     );
   };
 }
