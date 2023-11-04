316861040894ebd8274bb99e6d011a0a6f85e867
Josh Pollock <jopo@mit.edu>
2023-11-04 19:14:27 -0400

    fix jetpack compose

--- src/gradient.tsx
+++ src/gradient.tsx
@@ -1,9 +1,10 @@
+import { For } from "solid-js";
 import withBluefish from "./withBluefish";
 
 type ColorOffset = { offset: number; color: string };
 
 type GradientProps = {
-  name: string;
+  id: string;
   colorOffsets: ColorOffset[];
   x1?: number;
   x2?: number;
@@ -11,25 +12,27 @@ type GradientProps = {
   y2?: number;
 };
 
-export const Gradient = withBluefish((props: GradientProps) => {
+export const Gradient = (props: GradientProps) => {
   return (
     <g>
       <linearGradient
-        id={props.name}
+        id={props.id}
         x1={props.x1}
         x2={props.x2}
         y1={props.y1}
         y2={props.y2}
       >
-        {props.colorOffsets.map((colorOffset) => (
-          <stop
-            offset={`${colorOffset.offset}%`}
-            stop-color={colorOffset.color}
-          />
-        ))}
+        <For each={props.colorOffsets}>
+          {(colorOffset) => (
+            <stop
+              offset={`${colorOffset.offset}%`}
+              stop-color={colorOffset.color}
+            />
+          )}
+        </For>
       </linearGradient>
     </g>
   );
-});
+};
 
 export default Gradient;
