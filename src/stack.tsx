2f5d12f189c304a010c5d5679ef34601772513e8
Josh Pollock <jopo@mit.edu>
2023-11-08 11:55:55 -0500

    implement stack. fix total

---
+++ src/stack.tsx
@@ -0,0 +1,57 @@
+import { withBluefish } from "./withBluefish";
+import {
+  ParentProps,
+  JSX,
+  splitProps,
+  mergeProps,
+  createMemo,
+  createEffect,
+} from "solid-js";
+import { StackArgs, stackLayout } from "./stackLayout";
+import { BBox, Transform, ChildNode } from "./scenegraph";
+import Layout from "./layout";
+
+export type StackProps = ParentProps<StackArgs>;
+
+export const Stack = withBluefish((props: StackProps) => {
+  // if both total and spacing are undefined, default spacing to 10
+  const spacing = createMemo(() => {
+    if (props.total === undefined && props.spacing === undefined) return 10;
+    return props.spacing;
+  });
+
+  props = mergeProps(
+    {
+      get spacing() {
+        return spacing();
+      },
+    },
+    props
+  );
+
+  const layout = (childNodes: ChildNode[]) => {
+    return stackLayout(props)(childNodes);
+  };
+
+  const paint = (paintProps: {
+    bbox: BBox;
+    transform: Transform;
+    children: JSX.Element;
+  }) => {
+    return (
+      <g
+        transform={`translate(${paintProps.transform.translate.x ?? 0}, ${
+          paintProps.transform.translate.y ?? 0
+        })`}
+      >
+        {paintProps.children}
+      </g>
+    );
+  };
+
+  return (
+    <Layout name={props.name} layout={layout} paint={paint}>
+      {props.children}
+    </Layout>
+  );
+});
--- src/stackv.tsx
+++ src/stackv.tsx
@@ -4,44 +4,20 @@ import withBluefish from "./withBluefish";
 import Layout from "./layout";
 import { BBox, Transform, ChildNode } from "./scenegraph";
 import { AlignmentHorizontal } from "./align";
+import { Stack } from "./stack";
 
 export type StackVProps = ParentProps<
   Omit<StackArgs, "direction"> & { alignment?: AlignmentHorizontal }
 >;
 
 export const StackV = withBluefish((props: StackVProps) => {
-  const layout = (childNodes: ChildNode[]) => {
-    const stackProps = mergeProps(
-      {
-        direction: "vertical" as const,
-        alignment: "centerX" as const,
-        spacing: 10,
-      },
-      props
-    );
-
-    return stackLayout(stackProps)(childNodes);
-  };
-
-  const paint = (paintProps: {
-    bbox: BBox;
-    transform: Transform;
-    children: JSX.Element;
-  }) => {
-    return (
-      <g
-        transform={`translate(${paintProps.transform.translate.x ?? 0}, ${
-          paintProps.transform.translate.y ?? 0
-        })`}
-      >
-        {paintProps.children}
-      </g>
-    );
-  };
-
-  return (
-    <Layout name={props.name} layout={layout} paint={paint}>
-      {props.children}
-    </Layout>
+  const stackProps = mergeProps(
+    {
+      direction: "vertical" as const,
+      alignment: "centerX" as const,
+    },
+    props
   );
+
+  return <Stack {...stackProps} />;
 });
