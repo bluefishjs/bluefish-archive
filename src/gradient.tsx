7623fb26fc25a5729d9a815c99c9f8fdbac3bf04
Josh Pollock <jopo@mit.edu>
2024-04-13 10:54:00 -0400

    fix gradient

--- src/gradient.tsx
+++ src/gradient.tsx
@@ -1,9 +1,12 @@
 import { For } from "solid-js";
 import withBluefish from "./withBluefish";
+import Layout from "./layout";
+import { BBox, Id, Transform } from "./scenegraph";
 
 type ColorOffset = { offset: number; color: string };
 
 type GradientProps = {
+  name: Id;
   id: string;
   colorOffsets: ColorOffset[];
   x1?: number;
@@ -12,27 +15,48 @@ type GradientProps = {
   y2?: number;
 };
 
-export const Gradient = (props: GradientProps) => {
-  return (
-    <g>
-      <linearGradient
-        id={props.id}
-        x1={props.x1}
-        x2={props.x2}
-        y1={props.y1}
-        y2={props.y2}
-      >
-        <For each={props.colorOffsets}>
-          {(colorOffset) => (
-            <stop
-              offset={`${colorOffset.offset}%`}
-              stop-color={colorOffset.color}
-            />
-          )}
-        </For>
-      </linearGradient>
-    </g>
-  );
-};
+export const Gradient = withBluefish(
+  (props: GradientProps) => {
+    const layout = () => {
+      return {
+        bbox: {
+          centerX: 0,
+          centerY: 0,
+          width: 0,
+          height: 0,
+        },
+        transform: {
+          translate: {},
+        },
+      };
+    };
+
+    const paint = (_paintProps: { bbox: BBox; transform: Transform }) => {
+      return (
+        <g>
+          <linearGradient
+            id={props.id}
+            x1={props.x1}
+            x2={props.x2}
+            y1={props.y1}
+            y2={props.y2}
+          >
+            <For each={props.colorOffsets}>
+              {(colorOffset) => (
+                <stop
+                  offset={`${colorOffset.offset}%`}
+                  stop-color={colorOffset.color}
+                />
+              )}
+            </For>
+          </linearGradient>
+        </g>
+      );
+    };
+
+    return <Layout name={props.name} layout={layout} paint={paint} />;
+  },
+  { displayName: "Gradient" }
+);
 
 export default Gradient;
